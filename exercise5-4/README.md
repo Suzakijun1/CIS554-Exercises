ef3f12845b46683d8d845d76e87fbf96b9b73548
Dylan Yong <dyong1997@gmail.com>
2024-08-06 19:34:34 -0400

    added exercise5-4

---
+++ exercise5-4/README.md
@@ -0,0 +1,10 @@
+## Title: Exercise 5_4 – Function templates
+
+## Objective: Allow the student to further explore function templates
+
+## Statement of Problem:
+
+Update the function template Maximum, shown in Session 5.8 (Fig.
+6.25 and Fig. 6.26) to include a 4th parameter. This will allow the user to get the maximum value
+of 4 values. Make sure you update the template in Fig. 6.25, as well as the program that uses it
+in Fig. 6.26
---
+++ exercise5-4/maximum.h
@@ -0,0 +1,26 @@
+// maximum.h
+template <typename T>
+T maximum(T value1, T value2, T value3, T value4)
+{
+    T maximumValue = value1; // assume value1 is maximum
+
+    // determine whether value2 is greater than maximumValue
+    if (value2 > maximumValue)
+    {
+        maximumValue = value2;
+    }
+
+    // determine whether value3 is greater than maximumValue
+    if (value3 > maximumValue)
+    {
+        maximumValue = value3;
+    }
+
+    // determine whether value4 is greater than maximumValue
+    if (value4 > maximumValue)
+    {
+        maximumValue = value4;
+    }
+
+    return maximumValue;
+}
